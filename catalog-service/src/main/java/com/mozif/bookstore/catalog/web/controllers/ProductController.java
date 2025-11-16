58939c839334ff0d9827b2fb0b0f1573bb64ed45
MozifBeigh <mmozif03@gmail.com>
2025-11-16 15:20:28 +0530

    Implementing Catalog Service

---
+++ catalog-service/src/main/java/com/mozif/bookstore/catalog/web/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.mozif.bookstore.catalog.web.controllers;
+
+import com.mozif.bookstore.catalog.domain.PagedResult;
+import com.mozif.bookstore.catalog.domain.Product;
+import com.mozif.bookstore.catalog.domain.ProductNotFoundException;
+import com.mozif.bookstore.catalog.domain.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController {
+
+    private final ProductService productService;
+
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
+    }
+
+    @GetMapping("/{code}")
+    ResponseEntity<Product> getProductByCode(@PathVariable String code){
+        return productService.getProductByCode(code)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> ProductNotFoundException.forCode(code));
+    }
+}
